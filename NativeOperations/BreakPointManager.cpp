2b6493e26ab99438ab2efeb86fd7275a8610ab20
YeonGyu Jeong <81887821@hanmail.net>
2020-04-07 20:07:32 +0900

    Updated instruction offset for FFXIV version 2020.03.27.0000.0000(5150346)

--- NativeOperations/BreakPointManager.cpp
+++ NativeOperations/BreakPointManager.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 const wstring ChatTranslator::NativeOperations::BreakPointManager::MODULE_NAME = L"ffxiv_dx11.exe";
-const size_t ChatTranslator::NativeOperations::BreakPointManager::VOICE_TARGET_OFFSET = 0xEA4EF6;
+const size_t ChatTranslator::NativeOperations::BreakPointManager::VOICE_TARGET_OFFSET = 0xEA5396;
 void* ChatTranslator::NativeOperations::BreakPointManager::voiceTargetInstructionAddress = nullptr;
 
 set<DWORD> ChatTranslator::NativeOperations::BreakPointManager::threadsInDebug = set<DWORD>();
